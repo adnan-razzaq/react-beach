89e52e74d6bbd985b721c53892fffc7999dc50b3
Adnan Razzaq <adnanrazzaq451@gmail.com>
2020-01-04 02:19:50 +0100

    FeaturedRoom Component

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,18 +1,25 @@
 import React, { Component } from "react";
 import { RoomContext } from "../context";
 import Loading from "./Loading";
+import Room from "./Room";
+import Title from "./Title";
 
 export default class Featured extends Component {
   static contextType = RoomContext;
   render() {
-    const { featuredRooms } = this.context;
-    console.log(featuredRooms);
+    let { loading, featuredRooms } = this.context;
+
+    featuredRooms = featuredRooms.map(room => {
+      return <Room key={room.id} room={room} />;
+    });
 
     return (
-      <div>
-        faeturd rooms
-        <Loading></Loading>
-      </div>
+      <section className="featured-rooms">
+        <Title title="Featured Rooms"></Title>
+        <div className="featured-rooms-center">
+          {loading ? <Loading /> : featuredRooms}
+        </div>
+      </section>
     );
   }
 }
---
+++ src/components/Room.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Room(room) {
+  return <section></section>;
+}
